d151ef384ab3d67e4aa05cd721cd3932716fa653
yunushangoksu <yunushangoksu@gmail.com>
2021-12-08 23:35:22 +0300

    added color palette

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+let currentColor = "white";
+
 const container = document.querySelector("#container");
 for (let j = 0; j < 16; j++) {
   for (let i = 0; i < 16; i++) {
-    const square = document.createElement("div");
+    let square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
-
-    
+    square.addEventListener("mouseover", function () {
+      square.style.backgroundColor = currentColor;
+    });
   }
 }
 
-document.querySelector(".square").addEventListener("mouseover", function () {
-  document.querySelector(".square").style.backgroundColor = "pink";
-});
+function palette(color) {
+  currentColor = color;
+}
